0f03770df882646929a74e19f48d2a50f70fafee
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-03-23 03:24:00 -0300

    feat: created startCalculation method on CarbonCalculatorService

--- src/main/java/br/com/actionlabs/carboncalc/service/CarbonCalculatorService.java
+++ src/main/java/br/com/actionlabs/carboncalc/service/CarbonCalculatorService.java
@@ -1,11 +1,32 @@
 package br.com.actionlabs.carboncalc.service;
 
 import br.com.actionlabs.carboncalc.dto.*;
+import br.com.actionlabs.carboncalc.model.Calculation;
+import br.com.actionlabs.carboncalc.repository.CalculationRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class CarbonCalculatorService {
-    
+    private final CalculationRepository calculationRepository;
+
     public StartCalcResponseDTO startCalculation(StartCalcRequestDTO request) {
+        Calculation calculation = new Calculation();
+
+        calculation.setName(request.getName());
+        calculation.setEmail(request.getEmail());
+        calculation.setPhoneNumber(request.getEmail());
+        calculation.setUf(request.getUf());
+
+        Calculation saved = calculationRepository.save(calculation);
+
         StartCalcResponseDTO response = new StartCalcResponseDTO();
+
+        response.setId(saved.getId());
         
         return response;
     }
