343067490827d9307247dc6d711752f1cd6d7862
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-03-23 05:01:16 -0300

    refactor: updated OpenRestController

--- src/main/java/br/com/actionlabs/carboncalc/rest/OpenRestController.java
+++ src/main/java/br/com/actionlabs/carboncalc/rest/OpenRestController.java
@@ -1,6 +1,7 @@
 package br.com.actionlabs.carboncalc.rest;
 
 import br.com.actionlabs.carboncalc.dto.*;
+import br.com.actionlabs.carboncalc.service.CarbonCalculatorService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -12,20 +13,23 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class OpenRestController {
 
+  private final CarbonCalculatorService carbonCalculatorService;
+
   @PostMapping("start-calc")
   public ResponseEntity<StartCalcResponseDTO> startCalculation(
       @RequestBody StartCalcRequestDTO request) {
-    throw new RuntimeException("Not implemented");
+
+    return ResponseEntity.ok(carbonCalculatorService.startCalculation(request));
   }
 
   @PutMapping("info")
   public ResponseEntity<UpdateCalcInfoResponseDTO> updateInfo(
       @RequestBody UpdateCalcInfoRequestDTO request) {
-    throw new RuntimeException("Not implemented");
+    return ResponseEntity.ok(carbonCalculatorService.updateInfo(request));
   }
 
   @GetMapping("result/{id}")
   public ResponseEntity<CarbonCalculationResultDTO> getResult(@PathVariable String id) {
-    throw new RuntimeException("Not implemented");
+    return ResponseEntity.ok(carbonCalculatorService.getResult(id));
   }
 }
